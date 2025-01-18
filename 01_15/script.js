57cfda481fb3a5a63efe7f6360c3c0b79ab56074
DarenaSkip <skiparius@gmail.com>
2025-01-18 13:54:33 +0200

    Antra uzduotis

--- 01_15/script.js
+++ 01_15/script.js
@@ -1,6 +1,6 @@
 // 1. Sukurkite funkciją, kuri sukuria atsitiktinių skaičių masyvą iš N elementų. Atsitiktiniai skaičiai nuo M iki O;
-// pvz: 
-// console.log(generuotiAtsitiktinius(4, 100, 105)); 
+// pvz:
+// console.log(generuotiAtsitiktinius(4, 100, 105));
 // atsakymas konsolėje: [101, 101, 100, 105]
 
 // function rand(min, max) {
@@ -17,13 +17,19 @@
 // }
 // console.log(atsitiktiniai);
 
-
 // 2. Sukurkite funkciją, kuri console.log’e parašo tik skaičius iš masyvo M, mažesnius nei N.
-// pvz: 
+// pvz:
 // tikMazesni([4,5,6,7,2,4], 5);
 // atsakymas konsolėje: 4, 2, 4
 
-// function
+// function tikMazesni(M, N) {
+//   for (let i = 0; i < M.length; i++) {
+//     if (M[i] < N) {
+//       console.log(M[i]);
+//     }
+//   }
+// }
+// tikMazesni([4, 5, 6, 7, 2, 4], 6);
 
 // 3. Sukurkite funkciją, countAllThrees(array), kuri suskaičiuotų, kiek kartų pateiktame skaičių masyve yra pasikartojantis skaičius 3;
 
@@ -41,7 +47,7 @@
 
 // function
 
-// 7. Sukurkite funkciją, kuri imituoja monetos metimą 20 kartų, suskaičiuokite kiek kartų iškrito skaičius, kiek kartų iškrito herbas. Paskaičiuokite procentaliai, kokiu dažnumu krito skaičius, kokiu dažnumu krito skaičius; 
+// 7. Sukurkite funkciją, kuri imituoja monetos metimą 20 kartų, suskaičiuokite kiek kartų iškrito skaičius, kiek kartų iškrito herbas. Paskaičiuokite procentaliai, kokiu dažnumu krito skaičius, kokiu dažnumu krito skaičius;
 // (pasiūlymas patobulinimui mini-game - > pridėti puslapyje mygtuką, leisiantį pasirinkti kokios pusės monetos tikitės, ir pridėti mesti monetą mygtukus, tada atitinkamai atvaizduoti, buvo laimėta - ar pralaimėta)
 
 // function
